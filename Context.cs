0af182d4811e388d6de9e4f9631cd7149ee07fcb
Seth Duncan <seth@seth-duncan.com>
2017-11-02 12:41:49 -0700

    Updating EF Model Context to rename and use connection string for DB

--- Context.cs
+++ Context.cs
@@ -11,6 +11,12 @@ namespace ComicBookGalleryModel
     public class Context : DbContext
     {
 
+        public Context()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+        }
+        
+        
         public DbSet<ComicBook> ComicBooks { get; set; }
 
     }
